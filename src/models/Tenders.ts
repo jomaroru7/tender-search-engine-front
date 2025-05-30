01a2d23c4bcb9b13198e65362571e0f7fb441842
Chema <jomaroru7@gmail.com>
2025-05-30 12:59:26 +0200

    Added service to get tenders WIP

---
+++ src/models/Tenders.ts
@@ -0,0 +1,11 @@
+export type getTendersRequest = {
+    "invoicing": number,
+    "place": string,
+    "activity": string
+}
+
+export type getTendersResponse = {
+    "invoicing": number,
+    "place": string,
+    "activity": string
+}
---
+++ src/services/Tenders.ts
@@ -0,0 +1,22 @@
+import type { getTendersRequest } from "../models/Tenders";
+
+const ENV = import.meta.env;
+
+
+export const getTenders = async ({ invoicing, place, activity }: getTendersRequest) => {
+    const response = await fetch(ENV.VITE_GET_TENDERS, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            invoicing,
+            place,
+            activity
+        }),
+    });
+
+    if (!response.ok) {
+        throw new Error(`Server error: ${response.status}`);
+    }
+
+    return await response.json();
+};
