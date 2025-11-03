e38a087292467f50d2443a3d93ef688388efbd19
Carlos Perales <sir.perales@gmail.com>
2025-11-03 12:12:46 +0100

    Small modification on the translations

--- src/views/LoginPage.tsx
+++ src/views/LoginPage.tsx
@@ -26,14 +26,14 @@ function LoginPage() {
       "Enter your new password": "Introduce tu nueva contraseña",
       "Enter your confirmation code": "Introduce tu código de confirmación",
       "We Emailed You": "Te hemos enviado un correo",
+      "Your code is on the way. To log in, enter the code we emailed to {username}. It may take a minute to arrive.":
+        "Tu código está en camino. Para iniciar sesión, introduce el código que te hemos enviado a {username}. Puede tardar un minuto en llegar.",
       "Your code is on the way. To log in, enter the code we emailed to { username }. It may take a minute to arrive.":
-        "Hemos enviado un código a { username }. Para iniciar sesión, introduce el código que te hemos enviado por correo. Puede tardar un minuto en llegar.",
+        "Tu código está en camino. Para iniciar sesión, introduce el código que te hemos enviado a { username }. Puede tardar un minuto en llegar.",
       "Your code is on the way. To log in, enter the code we emailed to {{ username }}. It may take a minute to arrive.":
-        "Hemos enviado un código a {{ username }}. Para iniciar sesión, introduce el código que te hemos enviado por correo. Puede tardar un minuto en llegar.",
-      "Your code is on the way. To log in, enter the code we emailed to ": "Código enviado. Para iniciar sesión, introduce el código que te hemos enviado por correo a ",
+        "Tu código está en camino. Para iniciar sesión, introduce el código que te hemos enviado a {{ username }}. Puede tardar un minuto en llegar.",
+      "Your code is on the way. To log in, enter the code we emailed to ": "Tu código está en camino. Para iniciar sesión, introduce el código que te hemos enviado a ",
       "It may take a minute to arrive.": "Puede tardar un minuto en llegar.",
-      "Your code is on the way. To log in, enter the code we emailed to {username}.":
-        "Hemos enviado un código a {username}. Para iniciar sesión, introduce el código que te hemos enviado por correo.",
       "We sent a code to": "Hemos enviado un código a",
       "Confirmation Code": "Código de confirmación",
       "User already exists": "El usuario ya existe",
